990f39ad84eb9e954291021a769edd71a6fd26dd
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-06 23:25:33 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.03 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int equal = 1;
+        int non_equal = -1;
+        if (num1 == num2)
+            return equal;
+        else
+            return non_equal;
+    }
+}
