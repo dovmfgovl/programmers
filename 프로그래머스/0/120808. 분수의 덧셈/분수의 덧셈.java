36d1c46abce24b1982396e139171c5abfc29cc6d
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-07 00:29:34 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.14 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(int numer1, int denom1, int numer2, int denom2) {       
+        // numer1, numer2 와 denom1, denom2 를 통분한다.
+        int numer = numer1*denom2 + numer2*denom1;
+        int denom = denom1*denom2;
+        
+        int max = 1;
+        
+        // 공약수를 max에 저장하면서 최종적으로 최대공약수가 max에 저장되도록 한다.
+        for (int i=1; i<=numer && i<=denom; i++) {
+            if (numer%i == 0 && denom%i == 0) {
+                max = i;
+            }   
+        } 
+        
+        numer = numer / max;
+        denom = denom / max;
+        
+        int[] answer = {numer, denom};
+        return answer;
+    }
+}
