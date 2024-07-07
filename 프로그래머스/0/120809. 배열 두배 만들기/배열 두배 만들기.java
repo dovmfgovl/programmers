64dd4b8ba2241fc9ef2ef6e9cfce3348e7f3703d
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-07 23:44:14 +0900

    Update 배열 두배 만들기.java

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -8,3 +8,15 @@ class Solution {
         return numbers;
     }
 }
+
+
+// stream을 사용한 문제풀이
+// Mapping : 스트림 내 요소들을 하나씩 특정 값으로 변환하는 작업. 값을 변환하기 위한 람다를 인자로 받는다.
+            // 스트림을 원하는 모양의 새로운 스트림으로 변환하고싶을 때 사용.
+import java.util.Arrays;
+
+class Solution2 {
+    public int[] solution(int[] numbers) {
+        return Arrays.stream(numbers).map(i -> i * 2).toArray();
+    }
+}
