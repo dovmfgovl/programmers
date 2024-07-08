17ff74a2dcba1c63f7da384ee799ba319de850ff
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-08 23:43:50 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int n) {
+        
+        int[] answer;
+        int odd = 0;
+        
+        // int 수에 따른 배열의 변화 : 짝수면 10/2, 홀수면 11/2 + 1
+        if (n % 2 == 0) {
+            answer = new int[n / 2];
+        } else {
+            answer = new int[n / 2 + 1];
+        }
+        
+        // for문을 돌려 배열에 값 넣기
+        for (int i=1; i <= answer.length; i++) {
+            answer[odd++] = i * 2 - 1;
+        }
+        
+        return answer;
+    }
+}
