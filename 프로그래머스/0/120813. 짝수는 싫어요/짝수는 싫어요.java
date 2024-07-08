e33e7f5496675ca7eda05160b931b10f30fa5516
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-08 23:47:07 +0900

    Update 짝수는 싫어요.java

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -28,3 +28,5 @@ import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int n) {
         return IntStream.rangeClosed(0, n).filter(value -> value % 2 == 1).toArray();
+    }
+}
