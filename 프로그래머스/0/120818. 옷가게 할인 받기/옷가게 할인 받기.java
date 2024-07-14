fec6f154ed36a12511d1b3706cc1eda90cc0e0d2
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-14 22:55:21 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -2,17 +2,17 @@ import java.lang.Math;
 
 class Solution {
     public int solution(int price) {
-        int answer = 0;
+        //int answer = 0;
         
-        if (price >= 500000)
-            answer = (int) Math.floor(price * 0.8);
-        else if (price >= 300000)
-            answer = (int) Math.floor(price * 0.9);
-        else if (price >= 100000)
-            answer = (int) Math.floor(price * 0.95);
-        else
-            answer = price;
+        //if (price >= 500000)
+          //  answer = (int) Math.floor(price * 0.8);
+        //else if (price >= 300000)
+          //  answer = (int) Math.floor(price * 0.9);
+        //else if (price >= 100000)
+          //  answer = (int) Math.floor(price * 0.95);
+        //else
+          //  answer = price;
         
-        return answer;
+        return (int) (price >= 500000 ? price * 0.8 : price >= 300000 ? price * 0.9 : price >= 100000 ? price * 0.95 : price);
     }
 }
