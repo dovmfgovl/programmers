ae5645a53bcbc17e524c6de4b4c223acdc94e46f
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-18 23:16:41 +0900

    [level 1] Title: 약수의 합, Time: 0.11 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -2,12 +2,12 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        for (int i=1; i <= n / 2; i++) {
+        for (int i=1; i <= n; i++) {
             if (n % i == 0) {
                 answer += i;
             }
         }
         
-        return answer + n;
+        return answer;
     }
 }
