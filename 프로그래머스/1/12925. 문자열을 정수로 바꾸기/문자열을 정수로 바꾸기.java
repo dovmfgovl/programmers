59651c2f6f088b57802424600cb51caca8c9b47e
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-19 22:36:17 +0900

    Update 문자열을 정수로 바꾸기.java

--- 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
+++ 프로그래머스/1/12925. 문자열을 정수로 바꾸기/문자열을 정수로 바꾸기.java
@@ -3,3 +3,25 @@ class Solution {
         return Integer.parseInt(s);
     }
 }
+
+// 다른 문제풀이
+public class StrToInt {
+    public int getStrToInt(String str) {
+            boolean Sign = true;
+            int result = 0;
+
+      for (int i = 0; i < str.length(); i++) {
+                char ch = str.charAt(i);
+                if (ch == '-')
+                    Sign = false;
+                else if(ch !='+')
+                    result = result * 10 + (ch - '0');
+            }
+            return Sign?1:-1 * result;
+    }
+    //아래는 테스트로 출력해 보기 위한 코드입니다.
+    public static void main(String args[]) {
+        StrToInt strToInt = new StrToInt();
+        System.out.println(strToInt.getStrToInt("-1234"));
+    }
+}
