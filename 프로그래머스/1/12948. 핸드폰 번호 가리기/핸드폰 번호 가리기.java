5d35666aa696a20b9662dcde6cdc901734f3598a
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-20 23:22:11 +0900

    Update 핸드폰 번호 가리기.java

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.java
@@ -16,3 +16,21 @@ class Solution {
         return new String(answer);
     }
 }
+
+// 다른 문제풀이 1 - toCharArray, valueOf
+class Solution {
+  public String solution(String phone_number) {
+     char[] ch = phone_number.toCharArray(); // 문자열을 문자 배열로 변환
+     for(int i = 0; i < ch.length - 4; i ++){
+         ch[i] = '*'; // 전화번호의 처음 부분을 '*'로 대체
+     }
+     return String.valueOf(ch); // 문자 배열을 문자열로 변환하여 반환
+  }
+}
+
+// 다른 문제풀이 2 - 정규식 전방긍정탐색
+class Solution {
+  public String solution(String phone_number) {
+    return phone_number.replaceAll(".(?=.{4})", "*");
+  }
+}
