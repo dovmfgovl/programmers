218bbc3f812785587c4f602a656d94455713ce79
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-21 19:12:01 +0900

    Update 자릿수 더하기.java

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -20,10 +20,10 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        char[] arr = Integer.toString(n).toCharArray();
+        char[] arr = Integer.toString(n).toCharArray(); // 정수를 문자열로 변환 -> 문자 배열로 변환
 
         for(int i = 0; i < arr.length; i++){
-            answer += arr[i] - 48;
+            answer += arr[i] - 48; // 문자 아스키 값에서 '0'의 아스키 값 48을 빼서 정수로 변환 (ex) 49 - 48 = 1) 후 합산
         }
 
         return answer;
@@ -37,10 +37,10 @@ public class Solution {
     public int solution(int n) {
         int answer = 0;
 
-        String num = String.valueOf(n);
+        String num = String.valueOf(n); // 정수를 문자열로 변환
         // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
         for(int i = 0 ; i < num.length() ; i ++){
-            answer += Integer.parseInt(String.valueOf(num.charAt(i)));
+            answer += Integer.parseInt(String.valueOf(num.charAt(i))); // 각 자릿수를 정수로 변환하여 합산
         }
 
         return answer;
@@ -53,9 +53,9 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int answer = 0;
-        String[] array = String.valueOf(n).split("");
+        String[] array = String.valueOf(n).split(""); // 정수를 문자열로 변환 후 각 자릿 수 분리
         for(String s : array){
-            answer += Integer.parseInt(s);
+            answer += Integer.parseInt(s); // 문자열을 다시 정수로 변환하여 합산
         }
         return answer;
     }
