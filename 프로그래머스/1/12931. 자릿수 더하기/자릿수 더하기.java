b1e2ac376a79c65dcda06a8008a49a433a70e2a3
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-21 18:51:19 +0900

    Update 자릿수 더하기.java

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -46,3 +46,17 @@ public class Solution {
         return answer;
     }
 }
+
+// 다른 문제풀이 3 : String 배열 변환
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String[] array = String.valueOf(n).split("");
+        for(String s : array){
+            answer += Integer.parseInt(s);
+        }
+        return answer;
+    }
+}
