9646d1067daf64f159a22aff32881f1e27ffc751
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-26 16:02:30 +0900

    Update 가운데 글자 가져오기.java

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.java
@@ -17,3 +17,15 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 문제풀이 - 3항 연산자
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        // s 길이
+        int length = s.length();
+        
+        return length % 2 == 0 ? s.substring(length/2-1, length/2+1) : s.substring(length/2, length/2+1);
+    }
+}
