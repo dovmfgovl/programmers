90dbf3501f38aa3091fbd000739943531cdf2f11
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-27 13:16:29 +0900

    Update 내적.java

--- 프로그래머스/1/70128. 내적/내적.java
+++ 프로그래머스/1/70128. 내적/내적.java
@@ -12,3 +12,23 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 문제풀이 1 - stream
+import java.util.stream.IntStream;
+
+class Solution {
+    public int solution(int[] a, int[] b) {
+        return IntStream.range(0, a.length).map(index -> a[index] * b[index]).sum();
+    }
+}
+
+// 다른 문제풀이 2 - for
+class Solution {
+    public int solution(int[] a, int[] b) {
+         int sum = 0;
+        for(int i=0;i<a.length;i++){
+            sum+= a[i]*b[i];
+        }
+        return sum;
+    }
+}
