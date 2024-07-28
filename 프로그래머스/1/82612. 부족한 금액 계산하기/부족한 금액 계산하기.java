c268d2f9e854654a28408d12fcc096518053c62b
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-28 18:59:27 +0900

    Update 부족한 금액 계산하기.java

--- 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -17,3 +17,25 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 문제풀이 1 - 등차수열의 합
+class Solution {
+    public long solution(int price, int money, int count) {      
+        return Math.max((long)price * count * (count+1) / 2 - money, 0);
+    }
+}
+
+// 다른 문제풀이 2 
+class Solution {
+
+    public long solution(int price, int money, int count) {
+
+        long answer = money;
+
+        for (int cnt = 0; cnt < count; ++cnt) {
+            answer -= (price * (cnt + 1));
+        }
+
+        return (answer > 0 ? 0 : -answer);
+    }
+}
