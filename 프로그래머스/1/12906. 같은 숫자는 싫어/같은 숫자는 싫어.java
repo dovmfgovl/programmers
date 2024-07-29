067b0c04ca3c47253e4a97eaecf5d9d8e5305b08
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-29 22:02:22 +0900

    Update 같은 숫자는 싫어.java

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -33,3 +33,21 @@ public class Solution {
         return answer;
     }
 }
+
+// 다른 문제풀이 1 - Stack
+import java.util.*;
+
+public class Solution {
+    public Stack<Integer> solution(int []arr) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        for(int num : arr){
+            if(stack.size() == 0 || stack.peek() != num){
+                stack.push(num);
+            }
+        }
+
+        return stack;
+    }
+}
