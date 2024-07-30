9704b64b6d58f231fbd59e9c1e5d9e0685433ad0
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-07-30 21:54:50 +0900

    Update 예산.java

--- 프로그래머스/1/12982. 예산/예산.java
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -19,3 +19,35 @@ class Solution {
         return answer;
     }
 }
+
+// 댜른 문제풀이 1 - 빼기 연산
+import java.util.*;
+
+class Solution {
+  public int solution(int[] d, int budget) {
+      int answer = 0;
+
+        Arrays.sort(d);
+
+        for (int i = 0; i < d.length; i++) {
+            budget -= d[i];
+
+            if (budget < 0) break;
+
+            answer++;
+        }
+
+        return answer;
+  }
+}
+
+// 다른 문제풀이 2 - while문
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int cnt = 0;
+        while((budget -= d[cnt]) >= 0 && ++cnt < d.length);
+        return cnt;
+    }
+}
