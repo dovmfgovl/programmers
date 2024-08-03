2c8b46756189f549b946ff7e7a563e1f087a6895
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-08-03 22:39:31 +0900

    Update 약수의 개수와 덧셈.java

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -24,3 +24,26 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 문제풀이 1 - 제곱수
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+
+        // left부터 right까지 모든 숫자에 대해 반복
+        for (int i = left; i <= right; i++) {
+            // i가 제곱수인지 확인
+            // Math.sqrt(i)는 i의 제곱근을 구함
+            // i가 제곱수라면, 제곱근을 정수로 나눴을 때 나머지가 0이 됨
+            if (i % Math.sqrt(i) == 0) {
+                // i가 제곱수인 경우, 약수의 개수가 홀수이므로 i를 빼줌
+                answer -= i;
+            } else {
+                // i가 제곱수가 아닌 경우, 약수의 개수가 짝수이므로 i를 더해줌
+                answer += i;
+            }
+        }
+
+        return answer;
+    }
+}
