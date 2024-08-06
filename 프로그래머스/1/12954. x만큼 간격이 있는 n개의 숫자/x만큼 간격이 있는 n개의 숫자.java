fdf8d67c5973c60c296c93520e73a85a254a905f
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-08-06 22:24:30 +0900

    Update x만큼 간격이 있는 n개의 숫자.java

--- 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
+++ 프로그래머스/1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.java
@@ -9,3 +9,27 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 문제풀이 1
+import java.util.stream.LongStream;
+class Solution {
+  public long[] solution(int x, int n) {
+      return LongStream.iterate(x, i->i+x).limit(n).toArray();
+  }
+}
+
+// 다른 문제풀이 2
+import java.util.*;
+class Solution {
+    public static long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        answer[0] = x;
+
+        for (int i = 1; i < n; i++) {
+            answer[i] = answer[i - 1] + x;
+        }
+
+        return answer;
+
+    }
+}
