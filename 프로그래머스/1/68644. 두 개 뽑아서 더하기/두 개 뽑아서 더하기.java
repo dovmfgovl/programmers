b7e53d26fce0611ca5eb9d5b98433afea47162c6
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-08-07 17:35:42 +0900

    Update 두 개 뽑아서 더하기.java

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -23,3 +23,30 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 문제풀이 1 - TreeSet을 사용하면 Arrays.sort로 오름차순 하지 않아도 됨
+import java.util.Iterator;
+import java.util.Set;
+import java.util.TreeSet;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        Set<Integer> sumNumber = new TreeSet();
+
+        for(int i = 0; i < numbers.length-1; i++){
+            for(int j = i+1; j < numbers.length; j++){
+                sumNumber.add(numbers[i] + numbers[j]);
+            }
+        }
+
+        int[] answer = new int[sumNumber.size()];
+        int index = 0;
+        Iterator itor = sumNumber.iterator();
+        while(itor.hasNext()){
+            answer[index] = (int)itor.next();
+            index++;
+        }
+
+        return answer;
+    }
+}
