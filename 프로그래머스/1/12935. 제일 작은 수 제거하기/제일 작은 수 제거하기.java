7ec3d425a8327e5e0604d386b8dad3ba8d818f63
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-08-12 22:15:41 +0900

    Update 제일 작은 수 제거하기.java

--- 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -25,3 +25,17 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 문제풀이 1
+import java.util.Arrays;
+import java.util.stream.Stream;
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+  public int[] solution(int[] arr) {
+      if (arr.length <= 1) return new int[]{ -1 };
+      int min = Arrays.stream(arr).min().getAsInt();
+      return Arrays.stream(arr).filter(i -> i != min).toArray();
+  }
+}
