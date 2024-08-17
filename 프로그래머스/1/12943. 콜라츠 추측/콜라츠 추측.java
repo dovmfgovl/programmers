93859dd3c0ce52fd4a8db79f30cb8d137c54caf1
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-08-17 19:30:26 +0900

    Update 콜라츠 추측.java

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -25,3 +25,22 @@ class Solution {
         return result;
     }
 }
+
+// 다른 문제풀이 1
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+
+        while(num != 1){
+            answer++;
+            if(answer==500)
+                return -1;
+            if(num%2==0){
+                num/=2;
+            }else{
+                num=num*3+1;
+            }
+        }
+        return answer;
+    }
+}
