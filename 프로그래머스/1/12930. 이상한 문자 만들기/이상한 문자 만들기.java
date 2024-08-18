934d49a73035fac8d97f5d2414502bf2a9dec5d1
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-08-18 23:10:12 +0900

    Update 이상한 문자 만들기.java

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -20,3 +20,36 @@ class Solution {
         return answer.toString();
     }
 }
+
+// 다른 문제 풀이 1
+class Solution {
+  public String solution(String s) {
+
+        String answer = "";
+        int cnt = 0;
+        String[] array = s.split("");
+
+        for(String ss : array) {
+            cnt = ss.contains(" ") ? 0 : cnt + 1;
+            answer += cnt%2 == 0 ? ss.toLowerCase() : ss.toUpperCase(); 
+        }
+      return answer;
+  }
+}
+
+// 다른 문제 풀이 2
+class Solution {
+  public String solution(String s) {
+        char[] chars = s.toCharArray();
+        int idx = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == ' ')
+                idx = 0;
+            else
+                chars[i] = (idx++ % 2 == 0 ? Character.toUpperCase(chars[i]) : Character.toLowerCase(chars[i]));
+        }
+
+        return String.valueOf(chars);
+  }
+}
