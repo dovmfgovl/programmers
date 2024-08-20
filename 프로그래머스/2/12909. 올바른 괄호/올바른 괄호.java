af1b0f6ebf16919fa335232d33613b97b2e73a2f
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-08-20 20:33:30 +0900

    Update 올바른 괄호.java

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -34,3 +34,27 @@ class Solution {
         return stack.isEmpty();
     }
 }
+
+// 다른 문제풀이 1
+class Solution {
+    boolean solution(String s) {
+        boolean answer = false;
+        int count = 0;
+        for(int i = 0; i<s.length();i++){
+            if(s.charAt(i) == '('){
+                count++;
+            }
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+            if(count < 0){
+                break;
+            }
+        }
+        if(count == 0){
+            answer = true;
+        }
+
+        return answer;
+    }
+}
