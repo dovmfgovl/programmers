0eaa4c19151e3640d6ad4ac871000b13df91d0e7
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-08-27 19:14:51 +0900

    Update 폰켓몬.java

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -19,3 +19,35 @@ class Solution {
         return kind;
     }
 }
+
+// 다른 문제풀이 1
+import java.util.HashSet;
+
+class Solution {
+    public int solution(int[] nums) {
+
+            HashSet<Integer> hs = new HashSet<>();
+
+            for(int i =0; i<nums.length;i++) {
+                hs.add(nums[i]);
+            }
+
+            if(hs.size()>nums.length/2)
+                return nums.length/2;
+
+            return hs.size();
+    }
+}
+
+// 다른 문제풀이 2
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+class Solution {
+    public int solution(int[] nums) {
+        return Arrays.stream(nums)
+                .boxed()
+                .collect(Collectors.collectingAndThen(Collectors.toSet(),
+                        phonekemons -> Integer.min(phonekemons.size(), nums.length / 2)));
+    }
+}
