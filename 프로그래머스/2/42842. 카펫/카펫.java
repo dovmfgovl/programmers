5ce67425bf2c8f8ac8add680ac1c59c3d9fdc2b8
dovmfgovl <136715201+dovmfgovl@users.noreply.github.com>
2024-08-29 22:18:29 +0900

    Update 카펫.java

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -23,3 +23,14 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 문제풀이 1
+import java.util.*;
+class Solution {
+    public int[] solution(int brown, int red) {
+        int a = (brown+4)/2;
+        int b = red+2*a-4;
+        int[] answer = {(int)(a+Math.sqrt(a*a-4*b))/2,(int)(a-Math.sqrt(a*a-4*b))/2};
+        return answer;
+    }
+}
